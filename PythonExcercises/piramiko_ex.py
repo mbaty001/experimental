03daceebce0ff567f18da0abcac1cdfc87cd9eca
Michal Batyra <michal.batyra@alcatel-lucent.com>
2014-11-07 15:01:09 +0100

    Add piramiko_ex

---
+++ PythonExcercises/piramiko_ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import piramiko
+
+ssh = piramiko.client.SSHClient()
+
+''' 
+The default policy is to reject all unknown servers - i.e. RejectPolicy is used
+AutoAddPolicy - automatically adds the hostname and new host key to the local HostKeys object.
+'''
+ssh.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+
+''' connect to the server '''
+ssh.connect(hostname='hostname', port=22, username='user', password='passwd')
+
+
+'''Execute a command on a host '''
+stdin, stdout, stderr = ssh.exec_command('df -k')
+
+print stdout.read()
+
+''' close connection '''
+ssh.close()
