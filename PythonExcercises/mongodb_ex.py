c0fc013f20a35526b8ba26cda2a54e9a00c1107f
mbaty001 <michal.batyra@gmail.com>
2014-11-22 22:12:14 +0100

    Saturday evening

--- PythonExcercises/mongodb_ex.py
+++ PythonExcercises/mongodb_ex.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
 import pymongo
+import argparse
 
-print 'no i ok:)'
+def parseArgs():
+    parser = argparse.ArgumentParser( description='MongoDB training', formatter_class=argparse.ArgumentDefaultsHelpFormatter )
+ 
+    parser.add_argument( '--host', action='store', default='localhost', help='MongoDB host' )
+    parser.add_argument( '--port', action='store', default=27017, help='MongoDB port')
+
+    return vars(parser.parse_args())
+
+if __name__ == '__main__':
+    args = parseArgs()
+    
+    client = pymongo.MongoClient(args['host'], args['port'])
+    
+    print client
