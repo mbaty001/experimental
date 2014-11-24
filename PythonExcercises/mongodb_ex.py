f23479ff7a1832e59154843f0fa8c375790f4f76
mbaty001 <michal.batyra@gmail.com>
2014-11-24 21:41:59 +0100

    Monday evening

--- PythonExcercises/mongodb_ex.py
+++ PythonExcercises/mongodb_ex.py
@@ -1,8 +1,56 @@
 #!/usr/bin/env python
 
+'''
+Start mangodb
+ cd /cygdrive/d/Research/MongoDB/mongodb-win32-i386-2.0.0/bin
+ ./mongod.exe --dbpath ../../data/db
+ 
+ mango <-- start shell
+ 1. create db:
+  use michal <-- mango will not create a db permament until you insert a data there
+  db <-- list current db
+  i = {'author' : 'John Cook'}
+  j = {'author' : 'Ali Baba'}
+  db.testData.insert(i) <-- insert a data into db michal
+  db.testData.insert(j)
+'''
 import pymongo
 import argparse
+import sys
 
+class DB(object):
+    ''' Generic class for handling MongoDB '''
+    
+    def __init__(self, db, host='localhost', port=27017):
+        self.host = host
+        self.port = port
+        self.client = pymongo.MongoClient(self.host, self.port)
+        self.db = self.client[db]
+        self.collections = self.db.collection_names() if not None else None
+      
+    def getDb(self):        
+        return self.db
+    
+    def getCollections(self):
+        return self.collections
+    
+    def checkCollection(self, collection):
+        return collection in self.collections
+    
+    def getAllData(self, collection):
+        try:    
+            return self.db[collection].find()
+        except Exception, err:
+            print 'getAllData: Cannot get any data (%s)' %err
+            sys.exit(1)
+    
+    def getData(self, collection, key, value):
+        try:
+            return self.db[collection].find_one({key : value})
+        except Exception, err:
+            print 'getData: Cannot get any data (%s)' %err
+            sys.exit(1)       
+    
 def parseArgs():
     parser = argparse.ArgumentParser( description='MongoDB training', formatter_class=argparse.ArgumentDefaultsHelpFormatter )
  
@@ -14,6 +62,15 @@ def parseArgs():
 if __name__ == '__main__':
     args = parseArgs()
     
-    client = pymongo.MongoClient(args['host'], args['port'])
+    client = DB(host=args['host'], port=args['port'], db='michal')                  
+    print client.getCollections()
     
-    print client
+    print client.checkCollection('testData')
+    
+    alldata = client.getAllData('testData')
+    print alldata
+    
+    onedata = client.getData('testData', 'author', 'John Cook')
+    print onedata 
+        
+    
