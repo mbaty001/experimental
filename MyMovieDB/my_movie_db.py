3c6a23cb870176314245d1c179d2753dcc03235e
mbaty001 <michal.batyra@gmail.com>
2015-02-03 21:00:02 +0100

    Ready for cherryPy integration.

--- MyMovieDB/my_movie_db.py
+++ MyMovieDB/my_movie_db.py
@@ -7,6 +7,7 @@ http://docs.python-requests.org/en/latest/user/quickstart/
 '''
 
 import json
+import operator
 import requests
 
 def print_json(what):        
@@ -17,10 +18,22 @@ class MovieDB(object):
         self.api_key = '580c2a10f40a0b812aab4679b004d413'
         self.url = 'http://api.themoviedb.org/3'
         self.args = {'api_key' : self.api_key}
-        self.popular = Request(self.url + '/movie/popular', self.args).get()['results'] or None
+        self.populars = Request(self.url + '/movie/popular', self.args).get()['results'] or None
+        self.sorted_populars_by_vote_average()
         
+    def sorted_populars_by_vote_average(self):
+        self.populars = sorted(self.populars, 
+                               key=operator.itemgetter('vote_average'),
+                               reverse = True)    
+            
     def get_movie(self, id):      
-        print_json(Request(self.url + '/movie/%s' %id, self.args).get())
+        dataDict = Request(self.url + '/movie/%s' %id, self.args).get()        
+        return {'title' : dataDict['title'], 
+               'vote_average' : dataDict['vote_average'], 
+               'production_countries' : dataDict['production_countries'], 
+               'poster_path' : 'https://image.tmdb.org/t/p/w130/' + dataDict['poster_path'],
+               'overview' : dataDict['overview'], 
+               'genres' : dataDict['genres']}
         "https://image.tmdb.org/t/p/w130/<poster_path>"
         
 
@@ -35,9 +48,10 @@ class Request(object):
 
 if __name__ == '__main__':
         
-    db = MovieDB()
-    for movie in db.popular:
-        db.get_movie(movie['id'])
+    db = MovieDB()   
+     
+    for movie in db.populars:
+        print db.get_movie(movie['id'])
         print 30*'*' 
 
     
