7829e6d71fa4a5c00bdf378d67d5ea238474ef58
Michal Batyra <michal.batyra@nokia.com>
2021-10-16 16:31:54 +0200

    design patterns - factory

---
+++ design-patterns/factory-method/main.py
@@ -0,0 +1,89 @@
+import json
+import xml.etree.ElementTree as etree
+import sqlite3
+
+class JSONDataExtractor:
+  def __init__(self, filepath):
+    self.data = dict()
+    with open(filepath, mode='r', encoding='utf-8') as f:
+        self.data = json.load(f)
+  @property
+  def parsed_data(self):
+    return self.data
+
+class XMLDataExtractor:
+  def __init__(self, filepath):
+    self.tree =  etree.parse(filepath)
+  @property
+  def parsed_data(self):
+    return self.tree        
+
+class SQLiteDataExtractor:
+    def __init__(self, filepath):
+        connection = sqlite3.connect(filepath)
+        cursor = connection.cursor()
+        self.data = cursor.execute('Select * from persons').fetchall()
+        connection.close()
+    @property
+    def parsed_data(self):
+        return self.data
+
+def dataextraction_factory(filepath):
+    if filepath.endswith('json'):
+        extractor = JSONDataExtractor
+    elif filepath.endswith('xml'):
+        extractor = XMLDataExtractor
+    elif filepath.endswith('db'):
+        extractor = SQLiteDataExtractor
+    else:
+        raise ValueError('Cannot extract data from {}'.format(filepath))
+    return extractor(filepath)
+
+def extract_data_from(filepath):
+    factory_obj = None
+    try:
+        factory_obj = dataextraction_factory(filepath)
+    except ValueError as e:
+        print(e)
+    return factory_obj    
+
+def main():
+    json_factory = extract_data_from('data/movies.json')
+    json_data = json_factory.parsed_data
+    print(f'Found: {len(json_data)} movies')
+    for movie in json_data:
+        print(f"Title: {movie['title']}")
+        year = movie['year']
+        if year:
+            print(f"Year: {year}")
+        director = movie['director']
+        if director:
+            print(f"Director: {director}")
+        genre = movie['genre']
+        if genre:
+            print(f"Genre: {genre}")
+        print()
+
+    xml_factory = extract_data_from('data/persons.xml')
+    xml_data = xml_factory.parsed_data
+    liars = xml_data.findall(f".//person[lastName='Liar']")
+    print(f'found: {len(liars)} persons')
+    for liar in liars:
+        firstname = liar.find('firstName').text
+        print(f'first name: {firstname}')
+        lastname = liar.find('lastName').text
+        print(f'last name: {lastname}')
+        [print(f"phone number ({p.attrib['type']}):", p.text) 
+        for p in liar.find('phoneNumbers')]
+        print()
+
+    sqlite_factory = extract_data_from('data/persons.db')
+    sql_data = sqlite_factory.parsed_data
+    for person in sql_data:
+        print(f'First name: {person[0]}')
+        print(f'Last name: {person[1]}')
+        print(f'Phone number: {person[2]}')
+        print()
+
+if __name__ == '__main__':
+    main()
