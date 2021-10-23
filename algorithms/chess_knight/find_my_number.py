1353f12bc02b6daa0325ab5e45e9cb51c616c63a
Michal Batyra <michal.batyra@nokia.com>
2021-10-23 13:28:00 +0200

    enhance chess knight

--- algorithms/chess_knight/find_my_number.py
+++ algorithms/chess_knight/find_my_number.py
@@ -5,45 +5,45 @@ Find My Number solution by michal.batyra@gmail.com
 MATRIX = [ ['A','B','C','D','E'], ['F','G','H','I','J'], ['K','L','M','N','O'], ['',1,2,3,'' ] ]
 ROWS = len(MATRIX)
 COLS = len(MATRIX[0])
-VOWELS = [ [x,y] for x in range(ROWS) for y in range(COLS) if MATRIX[x][y] and str(MATRIX[x][y]).lower() in 'aeiou' ]
-EMPTIES = [ [x,y] for x in range(ROWS) for y in range(COLS) if not MATRIX[x][y] ]
+VOWELS = { (x,y) for x in range(ROWS) for y in range(COLS) if MATRIX[x][y] and str(MATRIX[x][y]).lower() in 'aeiou' }
+EMPTIES = { (x,y) for x in range(ROWS) for y in range(COLS) if not MATRIX[x][y] }
+JUMPS = ((-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1))
 
-def find_possible_movements(start_point, key_sequence, total_key_sequences, no_of_vowels, length):
+def find_possible_movements(start_point, key_sequence_length, total_key_sequences, no_of_vowels, length):
     ''' count the key_sequences with the given length '''
 
-    if len(key_sequence) == length:
+    if key_sequence_length == length:
         return total_key_sequences + 1
 
     (x, y) = start_point
 
-    jumps = ((-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1))
-    for jump in jumps:
+    for jump in JUMPS:
         tx = x + jump[0]
         ty = y + jump[1]
         if check_if_move_is_possible(tx, ty):
-            if no_of_vowels < 2 or [tx, ty] not in VOWELS:
-                key_sequence_copy = key_sequence + [(tx, ty)]
-                if [tx, ty] in VOWELS:
-                    total_key_sequences = find_possible_movements((tx, ty), key_sequence_copy, total_key_sequences, no_of_vowels+1, length)
-                else:
-                    total_key_sequences = find_possible_movements((tx, ty), key_sequence_copy, total_key_sequences, no_of_vowels, length)
+            if no_of_vowels < 2 or (tx, ty) not in VOWELS:
+                total_key_sequences = find_possible_movements((tx, ty), 
+                                                              key_sequence_length+1,
+                                                              total_key_sequences,
+                                                              no_of_vowels+int((tx, ty) in VOWELS),
+                                                              length)
     return total_key_sequences
 
 def check_if_move_is_possible(tx, ty):
     ''' Verify if the move is not outside of chessboard '''
-    return 0<=tx<ROWS and 0<=ty<COLS and [tx, ty] not in EMPTIES
+    return 0<=tx<ROWS and 0<=ty<COLS and (tx, ty) not in EMPTIES
 
 def main(length):
     ''' entrypoint '''
     total_key_sequences = 0
     for startx in range(ROWS):
         for starty in range(COLS):
-            start_point = [startx, starty]
+            start_point = (startx, starty)
             if start_point in EMPTIES:
                 continue
             no_of_vowels = 1 if start_point in VOWELS else 0
-            key_sequence = [start_point]
-            total_key_sequences += find_possible_movements(start_point, key_sequence, 0, no_of_vowels, length)
+            key_sequence_length = 1
+            total_key_sequences += find_possible_movements(start_point, key_sequence_length, 0, no_of_vowels, length)
     return total_key_sequences
 
 if __name__ == "__main__":
--- algorithms/chess_knight/test_find_my_number.py
+++ algorithms/chess_knight/test_find_my_number.py
@@ -1,22 +1,16 @@
-from find_my_number import check_if_move_is_possible, find_possible_movements, main
-
-matrix = [ ['A','B','C','D','E'], ['F','G','H','I','J'], ['K','L','M','N','O'], ['',1,2,3,'' ] ]
-rows = len(matrix)
-cols = len(matrix[0])
-vowels = [ [x,y] for x in range(rows) for y in range(cols) if matrix[x][y] and str(matrix[x][y]).lower() in 'aeiou' ]
-empties = [ [x,y] for x in range(rows) for y in range(cols) if not matrix[x][y] ]
+from find_my_number import check_if_move_is_possible, find_possible_movements, main, ROWS, COLS
 
 def test_check_if_move_is_possible():
     assert check_if_move_is_possible(0, 0) == True
     assert check_if_move_is_possible(3, 0) == False
-    assert check_if_move_is_possible(rows+1, 2) == False
-    assert check_if_move_is_possible(1, cols+2) == False
+    assert check_if_move_is_possible(ROWS+1, 2) == False
+    assert check_if_move_is_possible(1, COLS+2) == False
     assert check_if_move_is_possible(-1, 2) == False
     
 def test_find_possible_movements():
-    assert find_possible_movements(start_point=(0,0), key_sequence=[(0,0)], total_key_sequences=0, no_of_vowels=1, length=1) == 1
-    assert find_possible_movements(start_point=(0,0), key_sequence=[(0,0)], total_key_sequences=0, no_of_vowels=1, length=2) == 2
-    assert find_possible_movements(start_point=(0,1), key_sequence=[(0,1)], total_key_sequences=0, no_of_vowels=0, length=2) == 3
+    assert find_possible_movements(start_point=(0,0), key_sequence_length=1, total_key_sequences=0, no_of_vowels=1, length=1) == 1
+    assert find_possible_movements(start_point=(0,0), key_sequence_length=1, total_key_sequences=0, no_of_vowels=1, length=2) == 2
+    assert find_possible_movements(start_point=(0,1), key_sequence_length=1, total_key_sequences=0, no_of_vowels=0, length=2) == 3
 
 def test_main():
     assert main(1) == 18
