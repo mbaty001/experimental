62fe26af57e8bf5fa4334d48b42c8e4a78e1de45
Michal Batyra <michal.batyra@nokia.com>
2021-11-04 15:53:30 +0100

    more silnias

--- algorithms/basics/silnia.py
+++ algorithms/basics/silnia.py
@@ -1,5 +1,24 @@
+import time
+
+# recurent, sloooow
 def factorial(n): 
   """returns n!"""
   return 1 if n < 2 else n * factorial(n - 1)
 
-print(factorial(10))
+from functools import reduce
+from operator import mul
+
+#reduce - nonrecurent - fast
+def factorial_reduce(n):
+    return reduce(mul, range(1, n+1))
+
+start = time.time()
+print(factorial(100))
+stop = time.time()
+print(stop - start)
+
+start = time.time()
+print(factorial_reduce(100))
+stop = time.time()
+print(stop - start)
+
