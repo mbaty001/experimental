086c93eb9aa6d2dd7c3599af6bad759d0bb05793
Michal Batyra <michal_batyra@epam.com>
2022-01-31 15:47:19 +0100

    yagc

---
+++ algorithms/basics/two-maximum.py
@@ -0,0 +1,15 @@
+from cmath import inf
+
+
+l = [-10, 3,21,2,45,4365, 54]
+
+max_one = -inf
+max_two = -inf
+
+for el in l:
+    if el > max_one:
+        max_two, max_one = max_one, el
+    elif el > max_two:
+        max_two = el
+
+print(f"{max_one}, {max_two}")
