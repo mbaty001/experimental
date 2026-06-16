fcef74b020b74715c705fc052845f9638572a5ac
Michal Batyra <michal_batyra@epam.com>
2026-06-16 15:37:03 +0200

    Streamlit

---
+++ streamlit-project-multipage/entrypoint.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Multi-Page App",
+    page_icon="🚀"
+)
+
+st.write("Entrypoint")
+
+st.sidebar.success("Select a page above.")
